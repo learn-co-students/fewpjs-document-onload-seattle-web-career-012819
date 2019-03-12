5f0c5c21c06dbe94471efc23781b96d53c79389c
github username <github email address>
2019-03-11 19:36:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   console.log("The DOM has loaded");
+   document.getElementById('text').textContent = "This is really cool!";
+ });
+
+ console.log(
+   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+ );
+
+//  function addingEventListener() {
+//    const main = document.getElementById('input');
+    
+//    main.addEventListener('click', function(event) {
+//      alert('I was clicked!');
+//    });
+//    };
+
+// function displayMessage(arg) {
+//    document.getElementById("greeting").textContent = arg
+//  }
+   
